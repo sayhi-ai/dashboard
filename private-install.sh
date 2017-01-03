39cd9dbb9fe69c6a51bb6b7494e6dbd0e1ee310d
Julian <julianbrendl@gmail.com>
2017-01-03 15:30:59 +0100

    test

--- private-install.sh
+++ private-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Installing dashboard-middleware.."
-#npm install dashboard-middleware --registry https://npm-proxy.fury.io/SVB7saLmv2583QUpksLX/sayhi-ai/
+npm install dashboard-middleware --registry https://npm-proxy.fury.io/SVB7saLmv2583QUpksLX/sayhi-ai/
 echo "done"
