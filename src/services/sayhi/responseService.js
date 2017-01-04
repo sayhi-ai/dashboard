4206b7deb71e84502f2d75f7721a1324839260d2
Julian <julianbrendl@gmail.com>
2017-01-04 09:58:14 +0100

    added escaping now anyway

--- src/services/sayhi/responseService.js
+++ src/services/sayhi/responseService.js
@@ -15,12 +15,12 @@ export const addResponse = function (phraseId, text, html, vars) {
   const token = localStorage.getItem('sayhi-jwt');
   return middleware.getResponseHandler().addResponse(token, phraseId, text, html, vars)
     .then(json => {
-        if (json.added) {
-          actions.addResponse({id: json.id, text: text})
-        } else {
-          handleDashboardError("Response already exists.")
-        }
-      })
+      if (json.added) {
+        actions.addResponse({id: json.id, text: text})
+      } else {
+        handleDashboardError("Response already exists.")
+      }
+    })
     .catch(error => {
       handleDashboardError("Unable to add response.")
     })
@@ -30,12 +30,12 @@ export const removeResponse = function (phraseId, responseId) {
   const token = localStorage.getItem('sayhi-jwt')
   return middleware.getResponseHandler().removeResponse(token, phraseId, responseId)
     .then(json => {
-        if (json.removed) {
-          actions.removeResponse(responseId)
-        } else {
-          handleDashboardError("Error removing response.")
-        }
-      })
+      if (json.removed) {
+        actions.removeResponse(responseId)
+      } else {
+        handleDashboardError("Error removing response.")
+      }
+    })
     .catch(error => {
       handleDashboardError("Unable to remove response.")
     })
