da318cffee15b00758e5232dbbc1dffeac9ae853
Julian <julianbrendl@gmail.com>
2017-01-14 17:39:50 +0100

    improved the general styling

--- src/components/dashboard/content/response/Response.js
+++ src/components/dashboard/content/response/Response.js
@@ -77,10 +77,11 @@ export default class Response extends React.Component {
     return (
       <div className='flex justify-between items-center hide-child'>
         <div
-          className='br4 white pointer pv2 ph3 ma1 f5'
+          className='br4 white pointer pv2 ph3 ma1 mt2 mb0 f5'
           style={{
             backgroundColor: '#19A5E4',
-            wordBreak: 'break-all'
+            wordBreak: 'break-all',
+            borderBottomLeftRadius: 0
           }}
         >
           {htmlResponse}
